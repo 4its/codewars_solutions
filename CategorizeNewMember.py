30040622a2f9aa53fdf9af728a9a3cdb5bc93d0b
Georgii Egiazarian <sliminc@ya.ru>
2023-09-24 22:43:51 +0300

    Add few solutions 24.09.2023

--- CategorizeNewMember.py
+++ CategorizeNewMember.py
@@ -1,7 +1,8 @@
 def open_or_senior(data):
-    return ['Senior'  if age >=55 and handi > 7 else 'Open' for (age,handi) in data ]
+    return ['Senior' if age >= 55 and handi > 7 else 'Open' for (age, handi)
+            in data]
 
 
 if __name__ == '__main__':
-    print(open_or_senior([(45, 12),(55,21),(19, -2),(104, 20)]))
-    print(open_or_senior([(59, 12),(55,-1),(12, -2),(12, 12)]))
+    print(open_or_senior([(45, 12), (55, 21), (19, -2), (104, 20)]))
+    print(open_or_senior([(59, 12), (55, -1), (12, -2), (12, 12)]))
